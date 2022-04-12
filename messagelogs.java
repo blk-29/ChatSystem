b448d64cedea0c878f0dd50ad74ca1db934fc7df
blk-29 <77765687+blk-29@users.noreply.github.com>
2022-04-12 13:19:19 -0700

    Create messagelogs.java

---
+++ messagelogs.java
@@ -0,0 +1,51 @@
+public class messagelogs {
+  /// this is what i have for message logs for now
+	private Message m;
+	private String GUC;
+	private String GC;
+
+	
+	public messagelogs(Message m) {
+		super();
+		this.m = m;
+	}
+
+
+	public Message getM() {
+		return m;
+	}
+
+
+	public void setM(Message m) {
+		this.m = m;
+	}
+	public void LogMessage() {
+		
+	}
+	public void sendLogs() {
+		
+	}
+
+	public String getGC() {
+		return GC;
+	}
+
+
+	public void setGC(String gC) {
+		GC = gC;
+	}
+
+
+	public String getGUC() {
+		return GUC;
+	}
+
+
+	public void setGUC(String gUC) {
+		GUC = gUC;
+	}
+	
+
+	
+
+}
